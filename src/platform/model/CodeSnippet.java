e6309f0fc9ba6c6d2081a5fcdd28974eaf4a73a4
kukinpower <thekukinpower@gmail.com>
2021-04-12 22:14:59 +0300

    add code highlighting

--- src/platform/model/CodeSnippet.java
+++ src/platform/model/CodeSnippet.java
@@ -2,18 +2,34 @@ package platform.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonGetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonSetter;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import platform.service.CodeSnippetsService;
 
+@Entity(name = "snippet")
+//@Table(name = "snippets")
 public class CodeSnippet implements Comparable<CodeSnippet> {
 
+  @JsonIgnore
+  @Id
+  @Column
+  private long id;
+
+  @Column
   private String code;
+
+  @Column
   private LocalDateTime date;
+
 
   public CodeSnippet() {
     this.date = LocalDateTime.now();
@@ -49,6 +65,34 @@ public class CodeSnippet implements Comparable<CodeSnippet> {
   @Override
   public int compareTo(CodeSnippet codeSnippet) {
     return this.date.compareTo(codeSnippet.date);
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+
+    CodeSnippet that = (CodeSnippet) o;
+
+    if (id != that.id) {
+      return false;
+    }
+    if (code != null ? !code.equals(that.code) : that.code != null) {
+      return false;
+    }
+    return date != null ? date.equals(that.date) : that.date == null;
+  }
+
+  @Override
+  public int hashCode() {
+    int result = (int) (id ^ (id >>> 32));
+    result = 31 * result + (code != null ? code.hashCode() : 0);
+    result = 31 * result + (date != null ? date.hashCode() : 0);
+    return result;
   }
 
 }
